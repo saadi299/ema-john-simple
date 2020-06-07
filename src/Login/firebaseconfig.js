020011949e1233d702c6151a65c027a028cfd684
saadi299 <saadimohammed149@gmail.com>
2020-06-07 12:53:21 +0600

    adding file

---
+++ src/Login/firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANJFhiIoNV2DJw-0RuWYGuW32HOUlV2-M",
+    authDomain: "ema-john-299.firebaseapp.com",
+    databaseURL: "https://ema-john-299.firebaseio.com",
+    projectId: "ema-john-299",
+    storageBucket: "ema-john-299.appspot.com",
+    messagingSenderId: "808777861520",
+    appId: "1:808777861520:web:40d572e5f1bb5126238b15"
+  };
+
+  export default firebaseConfig;
